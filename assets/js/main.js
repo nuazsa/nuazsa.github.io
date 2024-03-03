18831d21d489f2f8a82610d625bd8b23ab8cad77
nuazsa <nurazissaputra@gmail.com>
2024-03-03 16:50:02 +0700

    update portofolio section with swiper library

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,22 @@ timeline(document.querySelectorAll('.timeline'), {
     visibleItems: 4,
     forceVerticalMode: 800
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+});
