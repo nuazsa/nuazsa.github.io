caf3185dce5baec154caeeda7d06d89f49a3cc03
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-06-18 21:57:03 +0700

    Add new logo image for M-Knows

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,19 +26,54 @@ timeline(document.querySelectorAll('.timeline'), {
 
 
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 20,
+    depth: 150,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
+    768: {
+      slidesPerView: "auto",
     },
+  }
 });
+
+const birthDate = new Date("2003-11-02");
+const today = new Date();
+
+let age = today.getFullYear() - birthDate.getFullYear();
+const hasBirthdayPassed = (
+today.getMonth() > birthDate.getMonth() ||
+(today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate())
+);
+
+if (!hasBirthdayPassed) {
+age--;
+}
+
+const paragraph = document.getElementById("intro");
+paragraph.innerHTML = `I'm Azis, a ${age}-year-old Full-stack developer & Backend Engineer. Currently pursuing a major in
+    Informatics Engineering at the University of Bina Sarana Informatika. With a passion for creating
+    and improving code, I focus on software development.`;
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById("footer").innerHTML = `© ${currentYear} Nur Azis Saputra. All Rights Reserved.`;
